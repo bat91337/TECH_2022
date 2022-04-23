ed713bcb759d5fd27d50334c41f149c67b353b42
d <d>
2022-04-23 14:55:23 +0300

    refactor: delete test

--- Kotiki-2/src/test/java/ru/itmo/kotiki/KotikiApplicationTests.java
+++ Kotiki-2/src/test/java/ru/itmo/kotiki/KotikiApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.itmo.kotiki;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KotikiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ru.itmo.kotiki;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KotikiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
